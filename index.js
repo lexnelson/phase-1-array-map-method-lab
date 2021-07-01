4a5fa539669a709f42d3c7d1dbf2bd2c7e887529
Lex Nelson <lexinelson597@gmail.com>
2021-07-01 17:29:18 -0400

    commit tree 9468027835b81640d4374227a186ef59e434fbb5

--- index.js
+++ index.js
@@ -10,7 +10,76 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+// const titleCased = () => {
+//   const word = tutorials.split(' ')
+//   console.log(word)
+// }
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCase = () => {
+//   const word = tutorials.map(string => string.split(' ')); debugger;
+//   word.map(letter => letter.charAt(0).toUpperCase + letter.slice(1).join(' '));
+//   return word;
+// }
+// const titleCase = tutorials.map(function(words) {
+//   return words.split(' ').map(letter => letter.charAt(0).toUpperCase + letter.slice(1)).join(' ')
+// })
+
+// const titleCased = () => {
+//   return tutorials.map(function(titles){
+//     return titles.split(' ').map(function(words){ return words.charAt(0).toUpperCase() + words.slice(1);
+//     })
+//   })
+// }
+// const titleCased = () => {
+//   let eachTitle = tutorials.map(elem => elem.split(' ')); 
+//   for (let word in eachTitle) {
+//     for (let i = 0; i < eachTitle[word].length; i++) {
+//       eachTitle[word][i] = eachTitle[word][i][0].toUpperCase() + eachTitle[word][i].substr(1);
+//     }
+//     tutorials[word] = eachTitle[word].join(' ');
+//   }
+//   return tutorials;
+//}
+// const titleCased = () => {
+//   let eachTitle = tutorials.map(elem => elem.split(' ').map(word => word.charAt(0).toUpperCase())
+//   ) 
+//   return eachTitle.join('')
+// }
+
+// const titleCased = () =>  {
+//   const newTitles = tutorials.map(words => words.split(" ").map(cap => cap.charAt(0).toUpperCase() + words.slice(1)))
+//   return newTitles.join(' ')
+// }
+const titleCased = () =>  {
+  return tutorials.map(words => words.split(' ').map(cap => 
+    cap.charAt(0).toUpperCase() + cap.slice(1)).join(' '))
+  }
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
+
+// const titleCased = () => {
+//   const splitStrings = tutorials.split(' ');
+//    newArray = splitStrings.map(function ){ }
+//   // splitStrings[0].toUpperCase
+//   return tutorials
+// }
+
+// const titleCased = () => {
+//   tutorials.map(function (element) {
+//     const words = element.split(" ");
+//     words.map((word) => {
+//       return word[0].toUpperCase() + word.substring(1);
+//     }).join(" ");
+//   })
+// }
+
+
+
+// const titleCased = () => {
+//  const cap = tutorials.split(" ")
+//  cap.map(first => first[0].toUpperCase() + first.substring(1)).join(" ")
+    
+// }
